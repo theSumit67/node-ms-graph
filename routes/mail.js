d0330312466b5e0102b7786037ab7ce132fdec15
Sumit Maurya <thesumit67@gmail.com>
2018-05-21 21:48:34 +0530

    Added mail ❌ delete option

--- routes/mail.js
+++ routes/mail.js
@@ -25,7 +25,7 @@ router.get('/', async function(req, res, next) {
       // Get the 10 newest messages from inbox
       const result = await client
       .api('/me/mailfolders/inbox/messages')
-      .top(10)
+      .top(100)
       .select('subject,from,receivedDateTime,isRead')
       .orderby('receivedDateTime DESC')
       .get();
@@ -38,11 +38,56 @@ router.get('/', async function(req, res, next) {
       parms.debug = JSON.stringify(err.body, null, 2);
       res.render('error', parms);
     }
-    
+
   } else {
     // Redirect to home
     res.redirect('/');
   }
 });
+
+router.get('/delete/message/:id', async function(req, res, next) {
+  let id = req.params.id;
+  console.log ( id );
+  if ( id ){
+    let parms = { title: 'Inbox', active: { inbox: true } };
+
+    const accessToken = await authHelper.getAccessToken(req.cookies, res);
+    const userName = req.cookies.graph_user_name;
+
+    if (accessToken && userName) {
+      parms.user = userName;
+
+      // Initialize Graph client
+      const client = graph.Client.init({
+        authProvider: (done) => {
+          done(null, accessToken);
+        }
+      });
+
+      try {
+        // Get the 10 newest messages from inbox
+        const result = await client
+        .api('/me/mailfolders/inbox/messages/'+ id)
+        .delete();
+
+        console.log ( result.value );
+        res.redirect('/mail');
+      } catch (err) {
+        parms.message = 'Error retrieving messages';
+        parms.error = { status: `${err.code}: ${err.message}` };
+        parms.debug = JSON.stringify(err.body, null, 2);
+        res.render('error', parms);
+      }
+
+    } else {
+      // Redirect to home
+      res.redirect('/');
+    }
+  } else {
+    // Redirect to home
+    res.redirect('/');
+  }
+})
+
 
 module.exports = router;
